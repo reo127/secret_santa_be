2c4f3db0b2f16d2f59f0599b3e8170b9351271aa
Rohan <reo127.0.0.1@gmail.com>
2025-01-24 13:17:53 +0530

    minor

--- database.js
+++ database.js
@@ -3,6 +3,9 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try { 
     await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      // Add these options for serverless compatibility
       serverSelectionTimeoutMS: 5000,
       retryWrites: true,
       w: 'majority'
