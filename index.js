195d80aab175f76ef54ef92bbf890e4d263e5039
Rohan <reo127.0.0.1@gmail.com>
2025-01-30 10:41:07 +0530

    remove db connection

--- index.js
+++ index.js
@@ -12,8 +12,6 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// connectDB();
-
 app.use('/api', santaRoutes);
 
 const PORT = process.env.PORT || 8000;
